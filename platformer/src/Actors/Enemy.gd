2f543639b4eba85ac4a933dd1fcc9d65c0f35f4d
christopherlim98 <christopherlim98@gmail.com>
2020-04-29 14:23:53 +0800

    Commit 1.0: First commit

---
+++ platformer/src/Actors/Enemy.gd
@@ -0,0 +1,27 @@
+extends "res://src/Actors/Actor.gd"
+
+func _ready() -> void:
+	_velocity.x = -speed.x
+	set_physics_process(false)
+	
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.get_name() != "Player":
+		return
+	if (body.global_position.y >
+	 get_node("StompDetector").global_position.y):
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+	
+func _physics_process(delta) -> void:
+	_velocity.y += gravity * delta
+	if is_on_wall():
+		_velocity *= -1.0
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+	
+	
+	
+
+
+
